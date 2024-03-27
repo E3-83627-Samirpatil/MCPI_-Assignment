310c46d741ee330e6d2c2d265ea3265adc348a2c
E3-83627-Samir <sameerpatil20178@gmail.com>
2024-03-27 18:13:33 +0530

    Assign_Day3

---
+++ Day03_1/Debug/Src/assignled.d
@@ -0,0 +1,14 @@
+Src/assignled.o: ../Src/assignled.c ../Src/Assig1_led.h \
+ ../Inc/stm32f407xx.h ../Inc/core_cm4.h ../Inc/cmsis_version.h \
+ ../Inc/cmsis_compiler.h ../Inc/cmsis_gcc.h ../Inc/mpu_armv7.h \
+ ../Inc/system_stm32f4xx.h ../Inc/stm32f4xx.h ../Inc/stm32f407xx.h
+../Src/Assig1_led.h:
+../Inc/stm32f407xx.h:
+../Inc/core_cm4.h:
+../Inc/cmsis_version.h:
+../Inc/cmsis_compiler.h:
+../Inc/cmsis_gcc.h:
+../Inc/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
+../Inc/stm32f4xx.h:
+../Inc/stm32f407xx.h:
---
+++ Day03_1/Src/Assig1_led.h
@@ -0,0 +1,26 @@
+/*
+ * Assig1_led.h
+ *
+ *  Created on: Mar 27, 2024
+ *      Author: sameer
+ */
+
+#ifndef ASSIG1_LED_H_
+#define ASSIG1_LED_H_
+
+#include "stm32f407xx.h"
+
+#define LED_GPIO	GPIOD
+#define LED_GPIO_EN	3
+
+#define LED_GREEN	12
+#define LED_ORANGE	13
+#define LED_RED		14
+#define LED_BLUE	15
+
+void LedInit(uint32_t pin);
+void LedOn(uint32_t pin);
+void LedOff(uint32_t pin);
+void LedBlink(uint32_t pin, uint32_t delay);
+
+#endif /* ASSIG1_LED_H_ */
